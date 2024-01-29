4d2b916f341ec48be23ad5b10d745275c5181c4a
meli122 <melika12329@gmail.com>
2024-01-29 14:31:51 -0500

    lablatouring with terraform

---
+++ terraform/virtualbox/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    virtualbox = {
+      source = "terra-farm/virtualbox"
+      version = "0.2.2-alpha.1"
+    }
+  }
+}
+
+# There are currently no configuration options for the provider itself.
+
+resource "virtualbox_vm" "node" {
+  count     = 1
+  name      = format("node-%02d", count.index + 1)
+  image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+  cpus      = 1
+  memory    = "1024 mib"
+  # user_data = file("${path.module}/user_data")
+
+  network_adapter {
+    type           = "bridge"
+    host_interface = "vboxnet1"
+  }
+}
+
+output "IPAddr" {
+  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)
+}
+
+# output "IPAddr_2" {
+#  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 2)
+#}
