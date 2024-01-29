f9759593729ac79ca729262bba0b932e067d5314
meli122 <melika12329@gmail.com>
2024-01-29 12:04:09 -0500

    mac set-up scripts

--- scripts/setup-mac.sh
+++ scripts/setup-mac.sh
@@ -1,1 +1,21 @@
-#!/bin/bash
+!#/bin/bash
+
+# installing kubectl
+brew install kubectl
+
+#installing kind
+brew install kind
+
+#installing docker
+brew install docker
+
+#installing terraform
+brew install terraform
+
+#installing ansible
+brew install ansible
+
+#install virtualbox
+brew install virtualbox
+
+
